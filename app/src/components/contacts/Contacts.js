112a37057f1de9947b231a2ee8cd562d9cbbc0d5
Renata Dzherhalova <rdzhergalova@gmail.com>
2020-06-02 11:14:35 +0300

    contacts page finished

--- app/src/components/contacts/Contacts.js
+++ app/src/components/contacts/Contacts.js
@@ -5,6 +5,7 @@ import Header from "../shared/Header";
 import Section from "../home/Section";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faMapMarkerAlt, faPhoneAlt} from "@fortawesome/free-solid-svg-icons";
+import {Link} from "react-router-dom";
 
 function Contacts() {
   return (
@@ -24,11 +25,17 @@ function Contacts() {
             <a href="mailto:rdzhergalova@gmail.com">rdzhergalova@gmail.com</a>
           </div>
         </div>
+        <div className="contacts__icons">
+          <Link to="https://t.me/@le_kalmique"><i className="fab fa-telegram-plane 4x contacts__icon"/></Link>
+          <Link to="https://twitter.com/le_kalmique"><i className="fab fa-twitter 4x contacts__icon"/></Link>
+          <Link to="https://www.facebook.com/renatadzherhalova/"><i className="fab fa-facebook 4x contacts__icon"/></Link>
+          <Link to="https://github.com/le-kalmique/"><i className="fab fa-github 4x contacts__icon"/></Link>
+        </div>
       </div>
       <div className="contacts__map">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2540.648376318622!2d30.45448841561582!3d50.44764987947497!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4ce9d7d09398d%3A0xa218cd21e7add3af!2z0JrQvtGA0L_Rg9GBIOKEljE1INCa0J_QhiDRltC8LtCG0LPQvtGA0Y8g0KHRltC60L7RgNGB0YzQutC-0LPQvg!5e0!3m2!1sen!2sua!4v1591051823113!5m2!1sen!2sua"
-          width="900" height="300" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false"
+          width="700" height="400" frameBorder="0" style={{border:0}} allowFullScreen="" aria-hidden="false"
           tabIndex="0"/>
       </div>
     </div>
